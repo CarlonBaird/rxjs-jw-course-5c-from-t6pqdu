dba96da9f2d80a8951fa4d5a0808f4213fa2fe68
CarlonBaird <carlyestrella@live.com>
2022-11-20 16:29:25 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 //from is used to convert other types into Observables
 //e.g It can convert an array into an Observable -> from([A,B,C])
 //also used to create an observable from Promise -> from(Promise)
+
+import { from } from 'rxjs';
+
+from(['Alice', 'Ben', 'Charlie']).subscribe({
+  next: (value) => console.log(value),
+  complete: () => console.log('Completed'),
+});
