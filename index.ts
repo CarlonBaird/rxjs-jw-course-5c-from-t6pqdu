cf13a9db8d1e762750a08af3474c05d5ec4b1c86
CarlonBaird <carlyestrella@live.com>
2022-11-20 16:25:57 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 //from is used to convert other types into Observables
 //e.g It can convert an array into an Observable -> from([A,B,C])
+//also used to create an observable from Promise -> from(Promise)
